22fecb09a11d7eb2267343484fb37f4d7300a5c6
Austin Dulay <dulayaustin@gmail.com>
2026-03-19 23:06:31 +0800

    Lint fixes

--- db/migrate/20260318162135_create_account_users.rb
+++ db/migrate/20260318162135_create_account_users.rb
@@ -6,6 +6,6 @@ class CreateAccountUsers < ActiveRecord::Migration[8.1]
 
       t.timestamps
     end
-    add_index :account_users, [:account_id, :user_id], unique: true
+    add_index :account_users, [ :account_id, :user_id ], unique: true
   end
 end
