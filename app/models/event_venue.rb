3176e47f8f97fbed032c4e461fc39047439e7406
Austin Dulay <dulayaustin@gmail.com>
2026-04-29 22:15:52 +0800

    EventVenue schema info

--- app/models/event_venue.rb
+++ app/models/event_venue.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: event_venues
+#
+#  id         :bigint           not null, primary key
+#  end_at     :datetime
+#  notes      :text
+#  role       :integer          not null
+#  start_at   :datetime
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  event_id   :bigint           not null
+#  venue_id   :bigint           not null
+#
+# Indexes
+#
+#  index_event_venues_on_event_id           (event_id)
+#  index_event_venues_on_event_id_and_role  (event_id,role) UNIQUE
+#  index_event_venues_on_venue_id           (venue_id)
+#
 class EventVenue < ApplicationRecord
   belongs_to :event
   belongs_to :venue
